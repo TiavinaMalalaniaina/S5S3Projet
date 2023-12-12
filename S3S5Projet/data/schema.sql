2895f02e1a1c7b34537b6be10cd40d96338a1709
TiavinaMalalaniaina <117308273+TiavinaMalalaniaina@users.noreply.github.com>
2023-12-12 12:36:19 +0300

    base

--- S3S5Projet/data/schema.sql
+++ S3S5Projet/data/schema.sql
@@ -1,8 +1,23 @@
-CREATE TABLE model(
-    id SERIAL PRIMARY KEY,
-    nom VARCHAR(50) NOT NULL,
-    dtn DATE NOT NULL,
-    salaire DOUBLE PRECISION NOT NULL,
-    heure TIME,
-    timestamp 
-)
+CREATE SCHEMA IF NOT EXISTS "public";
+
+CREATE  TABLE "public".materiel ( 
+	id                   serial  NOT NULL  ,
+	nom                  varchar(50)  NOT NULL  ,
+	CONSTRAINT pk_materiaux PRIMARY KEY ( id )
+ );
+
+CREATE  TABLE "public".meuble_type ( 
+	id                   serial  NOT NULL  ,
+	nom                  varchar(50)  NOT NULL  ,
+	CONSTRAINT pk_meuble_type PRIMARY KEY ( id )
+ );
+
+CREATE  TABLE "public".materiel_type ( 
+	id                   serial  NOT NULL  ,
+	materiel_id          integer  NOT NULL  ,
+	meuble_type_id       integer  NOT NULL  ,
+	CONSTRAINT pk_materiel_type PRIMARY KEY ( id ),
+	CONSTRAINT fk_materiel_type_materiel FOREIGN KEY ( materiel_id ) REFERENCES "public".materiel( id )   ,
+	CONSTRAINT fk_materiel_type_meuble_type FOREIGN KEY ( meuble_type_id ) REFERENCES "public".meuble_type( id )   
+ );
+
